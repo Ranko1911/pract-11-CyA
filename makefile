41cd5d52d1981293aa714f52927effeecd494d34
alu0101327679 <64937197+alu0101327679@users.noreply.github.com>
2023-01-11 18:54:20 +0000

    entrada como la quiere el profe

--- makefile
+++ makefile
@@ -1,5 +1,8 @@
 make: src/main.cc src/BIGINT.cc src/Karatsuba.cc src/file_manager.cc
 	g++ src/main.cc src/BIGINT.cc src/Karatsuba.cc src/file_manager.cc -o programa
+
+make2: src/main2.cc src/BIGINT.cc src/Karatsuba.cc src/file_manager.cc
+	g++ src/main2.cc src/BIGINT.cc src/Karatsuba.cc src/file_manager.cc -o programa2
 	
 clean: 
 	rm programa
--- src/main2.cc
+++ src/main2.cc
@@ -19,46 +19,17 @@ int main(int argc, char *argv[]) {
   BigInt first("12345678");
   BigInt second("87654321");
   FileManager file_manager;
+  string holder;
+  string data1;
+  string data2;
 
   if (argc == 1) {
-    std::cout << "No se ha introducido ningun argumento" << std::endl;
+    // std::cout << "No se ha introducido ningun argumento" << std::endl;
+    std::cin >> data1;
+    std::cin >> data2;
+    // std::cout << data1 << " " << data2 << std::endl;
 
-  } else if (argc == 2) {
-    std::cout << "No se ha introducido suficientes argumentos " << std::endl;
-  } else if (argc == 3) {
-    if (strcmp(argv[1], "-r") == 0) {
-      // std::cout << "Primer argumento: " << argv[1] << std::endl;
-      // std::cout << "Segundo argumento: " << argv[2] << std::endl;
-
-      auto start = high_resolution_clock::now();
-
-      int num_digitos = std::stoi(argv[2]);
-      std::cout << "num_digitos: " << num_digitos << std::endl;
-      std::string primer_numero = "";
-      std::string segundo_numero = "";
-      for (int i = 0; i < num_digitos; i++) {
-        int random = rand() % 10;
-        primer_numero += std::to_string(random);
-      }
-      for (int i = 0; i < num_digitos; i++) {
-        int random = rand() % 10;
-        segundo_numero += std::to_string(random);
-      }
-      std::cout << "first: " << primer_numero << std::endl;
-      std::cout << "second: " << segundo_numero << std::endl;
-
-      BigInt first(primer_numero);
-      BigInt second(segundo_numero);
-
-      temp = karatsuba.multiply(first, second);
-      auto stop = high_resolution_clock::now();
-      auto duration = duration_cast<microseconds>(stop - start);
-
-      std::cout << "first * second = " << temp << std::endl;
-      std::cout << "Time taken by function: " << duration.count()
-                << " microseconds" << std::endl;
-    } else {
-      auto start = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
       // file_manager.ReadFile(argv[1]);
       // std::string primer_fichero = file_manager.GetContent();
       // file_manager.SetContent("");
@@ -70,8 +41,8 @@ int main(int argc, char *argv[]) {
       // BigInt first(primer_fichero);
       // BigInt second(segundo_fichero);
 
-      BigInt first(argv[2]);
-      BigInt second(argv[3]);
+      BigInt first(data1);
+      BigInt second(data2);
 
       temp = karatsuba.multiply(first, second);
       auto stop = high_resolution_clock::now();
@@ -80,8 +51,7 @@ int main(int argc, char *argv[]) {
       std::cout << "first * second = " << temp << std::endl;
       std::cout << "Time taken by function: " << duration.count()
                 << " microseconds" << std::endl;
-    }
-  } else if (argc == 4) {
+  } else if (argc == 2) {
     if (strcmp(argv[1], "-k") == 0) {
       auto start = high_resolution_clock::now();
       // file_manager.ReadFile(argv[2]);
@@ -130,8 +100,39 @@ int main(int argc, char *argv[]) {
     } else {
       cout << "Argumento no valido" << endl;
     }
-  } else if (argc == 5) {
-    if (strcmp(argv[1], "-n") == 0) {
+  } else if (argc == 3) {
+    if (strcmp(argv[1], "-r") == 0) {
+      // std::cout << "Primer argumento: " << argv[1] << std::endl;
+      // std::cout << "Segundo argumento: " << argv[2] << std::endl;
+
+      auto start = high_resolution_clock::now();
+
+      int num_digitos = std::stoi(argv[2]);
+      std::cout << "num_digitos: " << num_digitos << std::endl;
+      std::string primer_numero = "";
+      std::string segundo_numero = "";
+      for (int i = 0; i < num_digitos; i++) {
+        int random = rand() % 10;
+        primer_numero += std::to_string(random);
+      }
+      for (int i = 0; i < num_digitos; i++) {
+        int random = rand() % 10;
+        segundo_numero += std::to_string(random);
+      }
+      std::cout << "first: " << primer_numero << std::endl;
+      std::cout << "second: " << segundo_numero << std::endl;
+
+      BigInt first(primer_numero);
+      BigInt second(segundo_numero);
+
+      temp = karatsuba.multiply(first, second);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<microseconds>(stop - start);
+
+      std::cout << "first * second = " << temp << std::endl;
+      std::cout << "Time taken by function: " << duration.count()
+                << " microseconds" << std::endl;
+    } else if (strcmp(argv[1], "-n") == 0) {
       // std::cout << "Argumento 1: " << argv[1] << std::endl;
       // std::cout << "Argumento 2: " << argv[2] << std::endl;
       // std::cout << "Argumento 3: " << argv[3] << std::endl;
@@ -179,7 +180,8 @@ int main(int argc, char *argv[]) {
     } else {
       cout << "Argumento no valido" << endl;
     }
+  
   } else {
-    std::cout << "Demasiados argumentos" << std::endl;
+    std::cout << "Argumentos incorrectos" << std::endl;
   }
 }
